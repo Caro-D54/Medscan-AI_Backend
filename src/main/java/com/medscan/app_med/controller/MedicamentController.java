4d293343c095413e0590dac2d6edbf6cd3780cf0
Caro-D54 <c.donaire@alumno.um.edu.ar>
2026-08-26 21:55:10 -0300

    refactor(controller): use constructor injection for MedicamentRepo in MedicamentController
    chore(env): clear sensitive information from .env.example

--- src/main/java/com/medscan/app_med/controller/MedicamentController.java
+++ src/main/java/com/medscan/app_med/controller/MedicamentController.java
@@ -2,7 +2,6 @@ package com.medscan.app_med.controller;
 
 import com.medscan.app_med.model.Medicament;
 import com.medscan.app_med.repository.MedicamentRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,16 +9,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/medicaments")
 public class MedicamentController {
-    @Autowired 
-    private MedicamentRepo medicamentRepo;
+
+    private final MedicamentRepo medicamentRepo;
+
+    public MedicamentController(MedicamentRepo medicamentRepo) {
+        this.medicamentRepo = medicamentRepo;
+    }
 
     @GetMapping
     public List<Medicament> getAllMedicaments() {
         return medicamentRepo.findAll();
     }
-        
-    @PostMapping public Medicament guardar(@RequestBody Medicament medicament) {
+
+    @PostMapping
+    public Medicament guardar(@RequestBody Medicament medicament) {
         return medicamentRepo.save(medicament);
     }
-    
 }
